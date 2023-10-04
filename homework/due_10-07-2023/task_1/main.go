eedec6fe3cac1626d21ff664ec8c35364f80a6d8
qqdzv <145340990+qqdzv@users.noreply.github.com>
2023-10-04 20:05:07 +0300

    Update main.go

--- homework/due_10-07-2023/task_1/main.go
+++ homework/due_10-07-2023/task_1/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func fib(a int) int {
+	switch {
+	case a == 0:
+		return 0
+	case a == 1:
+		return 1
+	default:
+		return fib(a-1) + fib(a-2)
+	}
+
+}
 func main() {
-	fmt.Println("i don't work yet")
+	fmt.Println(fib(0))
 }
