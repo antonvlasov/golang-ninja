dd1c054886535862bb402b84cc7d9c51bb5fa3c2
NikitaTrik <master.wotmaster@yandex.ru>
2023-10-05 14:20:53 +0200

    first

--- homework/due_10-07-2023/task_1/main.go
+++ homework/due_10-07-2023/task_1/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func fibonacci() func() int {
+	first := 0
+	second := 1
+	return func() int {
+		ans := first
+		first, second = second, first+second
+		return ans
+	}
+}
+
 func main() {
-	fmt.Println("i don't work yet")
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
